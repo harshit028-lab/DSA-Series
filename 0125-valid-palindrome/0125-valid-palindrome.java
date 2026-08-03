37aa55ec9ca310160897e35b941341547c364557
Harshit Dubey <harshitdubey5011@gmail.com>
2026-08-03 10:36:14 +0530

    Time: 16 ms (13.46%), Space: 47.6 MB (10.22%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        return cleaned.equals(reversed);
+    }
+}
